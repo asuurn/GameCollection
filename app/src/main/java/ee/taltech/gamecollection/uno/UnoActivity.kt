8cf6604ad13f2d48f058262d165209ad3b401edd
asuurn <asuurn@taltech.ee>
2025-02-14 16:22:23 +0200

    Drunk uno done

--- app/src/main/java/ee/taltech/gamecollection/uno/UnoActivity.kt
+++ app/src/main/java/ee/taltech/gamecollection/uno/UnoActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import ee.taltech.gamecollection.MainActivity
 import ee.taltech.gamecollection.R
 
 class UnoActivity : AppCompatActivity() {
@@ -25,6 +26,20 @@ class UnoActivity : AppCompatActivity() {
         advancedButton.setOnClickListener {
             it.startAnimation(bounceAnimation)
             val intent = Intent(this, UnoAdvancedActivity::class.java)
+            startActivity(intent)
+        }
+
+        val drunkUnoButton: Button = findViewById(R.id.buttonUnoDrunk)
+        drunkUnoButton.setOnClickListener {
+            it.startAnimation(bounceAnimation)
+            val intent = Intent(this, UnoDrunkActivity::class.java)
+            startActivity(intent)
+        }
+
+        val backButton: Button = findViewById(R.id.buttonBackFromUno)
+        backButton.setOnClickListener {
+            it.startAnimation(bounceAnimation)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/ee/taltech/gamecollection/uno/UnoDrunkActivity.kt
+++ app/src/main/java/ee/taltech/gamecollection/uno/UnoDrunkActivity.kt
@@ -1,15 +1,12 @@
 package ee.taltech.gamecollection.uno
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ee.taltech.gamecollection.R
 
-class UnoDrinkingActivity : AppCompatActivity() {
+class UnoDrunkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_uno_drinking)
+        setContentView(R.layout.activity_uno_drunk)
     }
 }
