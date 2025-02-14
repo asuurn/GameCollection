41e2e4033979387ebac733aba0924d67c963f999
asuurn <asuurn@taltech.ee>
2025-02-14 13:04:20 +0200

    Basic uno done

--- app/src/main/java/ee/taltech/gamecollection/MainActivity.kt
+++ app/src/main/java/ee/taltech/gamecollection/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import ee.taltech.gamecollection.baila.BailaActivity
 import ee.taltech.gamecollection.paranoia.ParanoiaActivity
+import ee.taltech.gamecollection.uno.UnoActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +27,13 @@ class MainActivity : AppCompatActivity() {
         buttonToBaila.setOnClickListener {
             it.startAnimation(bounceAnimation)
             val intent = Intent(this, BailaActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonToUno: Button = findViewById(R.id.buttonToUno)
+        buttonToUno.setOnClickListener {
+            it.startAnimation(bounceAnimation)
+            val intent = Intent(this, UnoActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/ee/taltech/gamecollection/uno/UnoActivity.kt
+++ app/src/main/java/ee/taltech/gamecollection/uno/UnoActivity.kt
@@ -1,15 +1,24 @@
 package ee.taltech.gamecollection.uno
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ee.taltech.gamecollection.R
 
 class UnoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uno)
+
+        val bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce)
+
+        val basicButton: Button = findViewById(R.id.buttonUnoBasic)
+        basicButton.setOnClickListener {
+            it.startAnimation(bounceAnimation)
+            val intent = Intent(this, UnoBasicActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
